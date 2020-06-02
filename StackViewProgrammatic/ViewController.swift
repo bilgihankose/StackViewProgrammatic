99bdd442fb6398a7ae8415f64d6969e8b080d498
Bilgihan Kose <bilgihankose@gmail.com>
2020-06-02 20:58:19 +0300

    add stackview

--- StackViewProgrammatic/ViewController.swift
+++ StackViewProgrammatic/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let titleLabel = UILabel()
+    let stackView = UIStackView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,12 +19,47 @@ class ViewController: UIViewController {
     }
     
     func setupViews(){
-        let titleLabel = UILabel()
+        //label
         titleLabel.text = "Stack View Example"
         view.addSubview(titleLabel)
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30).isActive = true
         titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        //stackView
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 20
+        view.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 30).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -30).isActive = true
+        
+        let one = makeLabelForStackView(title: "Too late, my time has come")
+        stackView.addArrangedSubview(one)
+        
+        let two = makeLabelForStackView(title: "Sends shivers down my spine")
+        stackView.addArrangedSubview(two)
+        
+        let three = makeLabelForStackView(title: "Body's aching all the time")
+        stackView.addArrangedSubview(three)
+        
+        let four = makeLabelForStackView(title: "Goodbye everybody, I've got to go")
+        stackView.addArrangedSubview(four)
+        
+        let five = makeLabelForStackView(title: "Gotta leave you all behind and face the truth")
+        stackView.addArrangedSubview(five)
+    }
+    
+    func makeLabelForStackView(title: String) -> UILabel {
+        let svLabel = UILabel()
+        svLabel.text = title
+        svLabel.backgroundColor = .blue
+        svLabel.textColor = .white
+        svLabel.numberOfLines = 0
+        return svLabel
     }
     
 }
